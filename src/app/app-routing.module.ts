1b004c44c493a0d9807d112140695e90932359a3
Elmansouri <houssin.el.mansouri@gmail.com>
2020-11-01 18:42:56 +0100

    page add cleint/list dossier

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,13 +1,20 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { AccueilComponent } from './main/content/accueil/accueil.component';
+import { AddClientComponent } from './main/content/dossier/add-client/add-client.component';
 import { DossierComponent } from './main/content/dossier/dossier.component';
+import { ListDossierComponent } from './main/content/dossier/list-dossier/list-dossier.component';
 
 
 const routes: Routes = [
   { path: '', redirectTo: 'accueil', pathMatch: 'full' },
   { path: 'accueil', component: AccueilComponent },
-  { path: 'dossier', component: DossierComponent }
+  { path: 'dossier', component: DossierComponent,
+    children: [
+      { path: '', redirectTo: 'list-dossier', pathMatch: 'full' },
+      { path: 'list-dossier', component: ListDossierComponent },
+      { path: 'add-client', component: AddClientComponent }
+    ] }
 ];
 
 @NgModule({
--- src/app/main/content/dossier/dossier.module.ts
+++ src/app/main/content/dossier/dossier.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { DossierComponent } from './dossier.component';
 import { SharedModule } from '@core/modules/shared.module';
+import { AddClientComponent } from './add-client/add-client.component';
+import { ListDossierComponent } from './list-dossier/list-dossier.component';
 
 
 
 @NgModule({
-  declarations: [DossierComponent],
+  declarations: [DossierComponent, AddClientComponent, ListDossierComponent],
   imports: [
     SharedModule
   ]
